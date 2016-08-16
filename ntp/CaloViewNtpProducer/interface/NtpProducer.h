3739951a5e44858a0f20f8db00d2c97aeb4c0776
Christopher Brainerd <cbbrainerd@ucdavis.edu>
2016-08-16 18:45:21 -0500

    Committing this because I accidentally wiped some changes earlier (too tired)

--- ntp/CaloViewNtpProducer/interface/NtpProducer.h
+++ ntp/CaloViewNtpProducer/interface/NtpProducer.h
@@ -31,14 +31,17 @@
 
 template<typename... types> 
 class NtpProducer2 : edm::EDProducer {
+  typedef std::vector<std::pair<std::string, StringObjectFunction<typename C::value_type> > > tagType;
 public:
     NtpProducer2()=delete;
     NtpProducer2(const edm::ParameterSet&) {}
     virtual void produce( edm::Event& iEvent, const edm::EventSetup& iSetup) override {}
+    std::vector<std::pair<TypeID,tagType> > produces() { return std::vector<std::pair<TypeID,tagType> >(); }
 };
 
 template<typename C,typename type,typename... types>
 class NtpProducer2<C,type,types...> : public edm::EDProducer {
+  typedef std::vector<std::pair<std::string, StringObjectFunction<typename C::value_type> > > tagType;
 public:
   /// constructor from parameter set
   template<typename... strings>
@@ -49,36 +52,39 @@ public:
       lazyParser_( par.template getUntrackedParameter<bool>( "lazyParser", false ) ),
       prefix_( par.template getUntrackedParameter<std::string>( "prefix","" ) ),
       eventInfo_( par.template getUntrackedParameter<bool>( "eventInfo",true ) )
-  {
-      if(eventInfo_){
-        produces<edm::EventNumber_t>( prefix_+"EventNumber" ).setBranchAlias( prefix_ + "EventNumber" );
-        produces<unsigned int>( prefix_ + "RunNumber" ).setBranchAlias( prefix_ + "RunNumber" );
-        produces<unsigned int>( prefix_ + "LumiBlock" ).setBranchAlias( prefix_ + "Lumiblock" );
-      }
-            this->produces(par);
-   }
-      /// destructor
-//  NtpProducer2::~NtpProducer2() {}
-  
-  inline void produces(edm::ParameterSet& par) { //Called recursively so the main EDProducer registers ALL products. Kind of ugly. Inefficient? Also doesn't work lol
+      {
          std::vector<edm::ParameterSet> variables = par.template getParameter<std::vector<edm::ParameterSet> >("variables");
          std::vector<edm::ParameterSet>::const_iterator
          q = variables.begin(), end = variables.end();
+         if(eventInfo_){
+           produces<edm::EventNumber_t>( prefix_+"EventNumber" ).setBranchAlias( prefix_ + "EventNumber" );
+           produces<unsigned int>( prefix_ + "RunNumber" ).setBranchAlias( prefix_ + "RunNumber" );
+           produces<unsigned int>( prefix_ + "LumiBlock" ).setBranchAlias( prefix_ + "Lumiblock" );
+         }
          for (; q!=end; ++q) {
            if(q->getUntrackedParameter<std::string>("Ctype")!=typeString_)
                 continue;
            std::string tag = prefix_ + q->getUntrackedParameter<std::string>("tag");
            StringObjectFunction<typename C::value_type> quantity(q->getUntrackedParameter<std::string>("quantity"), lazyParser_);
            tags_.push_back(std::make_pair(tag, quantity));
-           produces<type>(tag).setBranchAlias(tag);
-           }
-         childProducer_.produces(par);
-  }
-  inline void delegateProduce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
-    this->produce(iEvent,iSetup);
-  }
-
-  protected:
+         }
+         allTags_=this->produces();
+         for(x=allTags_.begin();x!=allTags_.end();++x) {
+            for(y=x->second().begin();y!=x->second().end()) {
+                produces(x->first(),y->first()).setBranchAlias(y->first());
+            }
+         }
+      }
+      
+      std::vector<std::pair<TypeID,tagType> > produces() {
+            auto returnVal=childProducer_.produces();
+            returnVal.push_back(std::make_pair(TypeID(type),tags_);
+            return returnVal;
+      }
+      /// destructor
+//  NtpProducer2::~NtpProducer2() {}
+  
+  //protected: //Expose this publically so it be called from parent- check if this is the right way to do this. Alternatively maybe friend function or see how the FW itself calls a plugin
     /// process an event
   virtual void produce( edm::Event& iEvent, const edm::EventSetup& iSetup) override {
      edm::Handle<C> coll;
@@ -92,10 +98,9 @@ public:
          x->push_back(q->second(*elem));
        }
        iEvent.put(x, q->first);
-       childProducer_.delegateProduce(iEvent,iSetup);
+       childProducer_.produce(iEvent,iSetup);
      }
   }
-  
 
 private:
   /// label of the collection to be read in
@@ -103,7 +108,8 @@ private:
   std::string typeString_;
   edm::EDGetTokenT<C> srcToken_;
   /// variable tags
-  std::vector<std::pair<std::string, StringObjectFunction<typename C::value_type> > > tags_;
+  tagType tags_;
+  std::vector<std::pair<TypeID,tagType> > allTags_;
   bool lazyParser_;
   std::string prefix_;
   bool eventInfo_;
