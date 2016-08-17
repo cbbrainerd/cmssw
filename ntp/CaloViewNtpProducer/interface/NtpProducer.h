9048bcb1361f967a0c2d275024210944378aaeb9
Christopher Brainerd <cbbrainerd@ucdavis.edu>
2016-08-17 14:03:20 -0500

    Ntuple code fully functional. Removed debug code from python configuration files. Also removed redundacy of EventInfo being saved twice (once by each module)

--- ntp/CaloViewNtpProducer/interface/NtpProducer.h
+++ ntp/CaloViewNtpProducer/interface/NtpProducer.h
@@ -19,7 +19,7 @@
    * };
    * DEFINE_FWK_MODULE(MuonViewNtpProducer);
  * Might be worth finding a less ugly way to wrap this class, easily done as a macro
- * The string literals correspond to the "nice" python name used in config files
+ * The string literals correspond to the "nice" python name to be used in config files
  *
  */
 #include "FWCore/Framework/interface/EDProducer.h"
@@ -29,13 +29,13 @@
 #include "FWCore/Utilities/interface/InputTag.h"
 #include "CommonTools/Utils/interface/StringObjectFunction.h"
 
-template<typename C,typename... types> 
+template<typename C,typename... types> //Base class for the NtpProducer2 template. Every other NtpProducer2 has a child- this is the bottom one in the ladder. It's made specifically to not do much at all.
 class NtpProducer2 : edm::EDProducer {
   typedef std::vector<std::pair<std::string, StringObjectFunction<typename C::value_type> > > tagType;
 public:
     NtpProducer2()=delete;
-    NtpProducer2(const edm::ParameterSet&) {}
-    virtual void produce( edm::Event& iEvent, const edm::EventSetup& iSetup) override {}
+    NtpProducer2(bool,const edm::ParameterSet&) {}
+    virtual void produce( edm::Event& iEvent, const edm::EventSetup& iSetup) override {};
     std::vector<std::pair<edm::TypeID,tagType> > moduleProduces() { return std::vector<std::pair<edm::TypeID,tagType> >(); }
 };
 
@@ -46,33 +46,40 @@ public:
   /// constructor from parameter set
   template<typename... strings>
   NtpProducer2( const edm::ParameterSet& par , std::string typ, strings... otherTypes ) :
-      childProducer_(par,otherTypes...) ,
+      NtpProducer2(true,par,typ,otherTypes...) //Delegates to child constructor, then actually registers the products on top of that
+      {
+         if(eventInfo_){
+           produces<edm::EventNumber_t>( prefix_+"EventNumber" ).setBranchAlias( prefix_ + "EventNumber" );
+           produces<unsigned int>( prefix_ + "RunNumber" ).setBranchAlias( prefix_ + "RunNumber" );
+           produces<unsigned int>( prefix_ + "LumiBlock" ).setBranchAlias( prefix_ + "Lumiblock" );
+         }
+         allTags_=this->moduleProduces();
+         for(auto x=allTags_.begin();x!=allTags_.end();++x) {
+            for(auto y=x->second.begin();y!=x->second.end();++y) {
+                produces(x->first,y->first).setBranchAlias(y->first); //This registers all the products that all its children are capable of producing.
+            }
+         }
+      }
+
+  template<typename... strings> //Constructor for children. Don't register any products. The boolean argument is a dummy variable.
+  NtpProducer2( bool parent, const edm::ParameterSet& par , std::string typ, strings... otherTypes ) :
+      childProducer_(false,par,otherTypes...) ,
       typeString_(typ) ,
       srcToken_( consumes<C>( par.template getParameter<edm::InputTag>( "src" ) ) ),
       lazyParser_( par.template getUntrackedParameter<bool>( "lazyParser", false ) ),
       prefix_( par.template getUntrackedParameter<std::string>( "prefix","" ) ),
-      eventInfo_( par.template getUntrackedParameter<bool>( "eventInfo",true ) )
+      eventInfo_( par.template getUntrackedParameter<bool>( "eventInfo",true ) ),
+      parent_(parent)
       {
          std::vector<edm::ParameterSet> variables = par.template getParameter<std::vector<edm::ParameterSet> >("variables");
          std::vector<edm::ParameterSet>::const_iterator
          q = variables.begin(), end = variables.end();
-         if(eventInfo_){
-//           produces<edm::EventNumber_t>( prefix_+"EventNumber" ).setBranchAlias( prefix_ + "EventNumber" );
-//           produces<unsigned int>( prefix_ + "RunNumber" ).setBranchAlias( prefix_ + "RunNumber" );
-//           produces<unsigned int>( prefix_ + "LumiBlock" ).setBranchAlias( prefix_ + "Lumiblock" );
-         }
          for (; q!=end; ++q) {
            if(q->getUntrackedParameter<std::string>("Ctype")!=typeString_)
                 continue;
            std::string tag = prefix_ + q->getUntrackedParameter<std::string>("tag");
            StringObjectFunction<typename C::value_type> quantity(q->getUntrackedParameter<std::string>("quantity"), lazyParser_);
            tags_.push_back(std::make_pair(tag, quantity));
-         }
-         allTags_=this->moduleProduces();
-         for(auto x=allTags_.begin();x!=allTags_.end();++x) {
-            for(auto y=x->second.begin();y!=x->second.end();++y) {
-                produces(x->first,y->first).setBranchAlias(y->first);
-            }
          }
       }
       
@@ -87,7 +94,19 @@ public:
   
   //protected: //Expose this publically so it be called from parent- check if this is the right way to do this. Alternatively maybe friend function or see how the FW itself calls a plugin
     /// process an event
+
   virtual void produce( edm::Event& iEvent, const edm::EventSetup& iSetup) override {
+     if(eventInfo_ && parent_){ //Only records event info if it is requested AND it is the "parent"
+       std::auto_ptr<edm::EventNumber_t> event( new edm::EventNumber_t );
+       std::auto_ptr<unsigned int> run( new unsigned int );
+       std::auto_ptr<unsigned int> lumi( new unsigned int );
+       *event = iEvent.id().event();
+       *run = iEvent.id().run();
+       *lumi = iEvent.luminosityBlock();
+       iEvent.put( event, prefix_ + "EventNumber" );
+       iEvent.put( run, prefix_ + "RunNumber" );
+       iEvent.put( lumi, prefix_ + "LumiBlock" );
+     }
      edm::Handle<C> coll;
      iEvent.getByToken(srcToken_, coll);
      typename std::vector<std::pair<std::string, StringObjectFunction<typename C::value_type> > >::const_iterator
@@ -98,9 +117,9 @@ public:
        for (typename C::const_iterator elem=coll->begin(); elem!=coll->end(); ++elem ) {
          x->push_back(q->second(*elem));
        }
-       iEvent.put(x, q->first);
-       childProducer_.produce(iEvent,iSetup);
+       iEvent.put(x, q->first); //Puts the product in the event.
      }
+     childProducer_.produce(iEvent,iSetup);
   }
 
 private:
@@ -114,6 +133,7 @@ private:
   bool lazyParser_;
   std::string prefix_;
   bool eventInfo_;
+  bool parent_;
   //edm::EDProducer* childProducer_;
 };
 
