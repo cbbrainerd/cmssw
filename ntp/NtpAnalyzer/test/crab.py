80a4c1183d68354b500537b5a67abdade990947a
Christopher Brainerd <cbbrainerd@ucdavis.edu>
2016-10-06 15:31:27 -0500

    Fix

---
+++ ntp/NtpAnalyzer/test/crab.py
@@ -0,0 +1,96 @@
+#!/bin/env python
+#Crab wrapper script
+#Run as python crab.py crabCommand [version]
+#Do not use for crab submissions
+#Be sure to source /cvmfs/cms.cern.ch/crab3/crab.sh first
+import sys
+import subprocess
+import pickle
+import imp
+
+try:
+    imp.find_module('CRABAPI') #If CRAB setup script has not been sourced, exit without wasting any time
+except ImportError:
+    print 'CRAB environment has not been set up. Be sure to source the correct setup script (currently `source /cvmfs/cms.cern.ch/crab3/crab.sh` for bash.)'
+    raise SystemExit
+
+try:
+    f=open("versionControl.p",'r')
+    versions=pickle.load(f)
+    f.close()
+except Exception:
+    print 'Something went wrong. This is expected if `python submit_interactive.py` has not been run yet. Make sure that "versionControl.p" exists and not opened in any other process.'
+    raise
+
+if(len(sys.argv) == 1):
+    print 'This script requires at least one argument. Usage:'
+    print 'python crab.py crabCommand [crabOptions] [version (just a number)]'
+    exit()
+command=sys.argv[1]
+print sys.argv
+crabCmdOpts=[]
+if(len(sys.argv) > 2):
+    lastVersion=[]
+    for x in sys.argv[2:]:
+        try:
+            int(x)
+            versions['v'+str(x)]
+        except ValueError: #If it is not an integer, it must be a crab option. If it's not a valid one, let crab handle the error
+            crabCmdOpts.append(x)
+        except KeyError:
+            print 'Version v'+str(x)+' does not exist.'
+            raise SystemExit
+        else:
+            lastVersion.append(str(x))
+    if not lastVersion:
+        lastVersion=[len(versions)]
+else:
+    lastVersion=[len(versions)]
+lastHash=[]
+isMC=[]
+isData=[]
+MCDirectory=[]
+DataDirectory=[]
+print lastVersion
+for version in lastVersion:
+    try:
+        lastVersionInfo=versions['v'+str(version)]
+        lastHash.append(lastVersionInfo[0].rstrip())
+        isMC.append(lastVersionInfo[1] and lastVersionInfo[2])
+        isData.append(lastVersionInfo[3] and lastVersionInfo[4])
+        MCDirectory.append(lastVersionInfo[5])
+        DataDirectory.append(lastVersionInfo[6])
+    except KeyError:
+        print "Version \"v%s\" not found in dictionary. Please enter only one number for the first argument of this script, or no argument to resubmit the last submission." % version
+        raise SystemExit
+if (command=="submit"):
+    print 'Do not use this script to submit jobs.'
+    print 'Use python submit_interactive.py to submit jobs.'
+    print 'Now executing python submit_interactive.py'
+    subprocess.Popen("python submit_interactive.py",shell=True)
+    raise SystemExit
+print "Git hashes of given submits: %s." % lastHash
+print "Running command `crab %s%s` on %s." % (command,' '+' '.join(crabCmdOpts) if crabCmdOpts else '',("MC and Data" if (isMC and isData) else ("MC" if isMC else ("Data" if isData else "nothing."))))
+
+directory='./Ntupler/crab_'
+version='-v'+str(lastVersion)
+
+if __name__ == '__main__':
+    from CRABAPI.RawCommand import crabCommand
+    from CRABClient.ClientExceptions import ClientException
+    from httplib import HTTPException
+    
+    def job(directory,command,crabCmdOpts):
+        print 'Running command "crab %s%s":' % ((command+" -d "+directory),' '+' '.join(crabCmdOpts) if crabCmdOpts else '')
+        try:
+            crabCommand(command,d=directory,*crabCmdOpts)
+        except HTTPException as hte:
+            print "Failed submitting task: %s" % (hte.headers)
+        except ClientException as cle:
+            print "Failed submitting task: %s" % (cle)
+    print (isMC,isData,MCDirectory,DataDirectory)
+    for isMC_,isData_,MCDirectory_,DataDirectory_ in zip(isMC,isData,MCDirectory,DataDirectory):
+        if isMC_:
+            job(MCDirectory_,command,crabCmdOpts)
+        if isData_:
+            job(DataDirectory_,command,crabCmdOpts)
---
+++ ntp/NtpAnalyzer/test/submit_interactive.py
@@ -0,0 +1,107 @@
+#!/bin/env python
+#Submits crab jobs
+#Run as "python submit.py"
+#Be sure to source /cvmfs/cms.cern.ch/crab3/crab.sh
+
+import subprocess
+import pickle
+import re
+
+import sys
+
+skipGitStatusCheck=True #Default behavior. Can force the check with a flag
+
+crabCmdOpts=[]
+
+def parse(argument): #Parse command line arguments. Return false if they are not script-specific
+    if(argument=='--forceGitCheck' or argument=='-g'):
+        skipGitStatusCheck=False
+#    elif ():
+    else:   
+        return false
+
+for arg in sys.argv[1:]:
+    if not parse(arg):
+        cramCmdOpts.append(arg)
+
+class GenericSubmissionException(Exception):
+    def __init__(self,value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+try:
+    f=open("versionControl.p",'r')
+    versions=pickle.load(f)
+    newVersion=len(versions)+1
+    f.close()
+except (EOFError,IOError):
+    versions={}
+    newVersion=1
+
+if not skipGitStatusCheck:
+    try:
+        subprocess.check_call(["git","status","-uno"])
+    except subprocess.CalledProcessError:
+        print "Uncommitted changes to tracked files."
+        exit()
+
+def interactiveOptionsSubmit(config,runType,dataEntry=False):
+    allGood=True
+    dataset=''
+    if dataEntry:
+        dataset=raw_input("Type the dataset to run over if it is not default for the "+runType+" run.\n")
+    if(dataset!=''):
+        try:
+            crab.changeDataset(config,dataset)
+        except InvalidDatasetException:
+            allGood=False
+    if allGood:
+        print "Submitting",runType,"crab job..."
+        try:
+            submit(config,crabCmdOpts)
+        except GenericSubmissionException:
+            print runType,"submission failed."
+            allGood=False
+    return "./"+config.General.workArea+"/crab_"+config.General.requestName if allGood else ''
+
+newHash=subprocess.check_output(["git","rev-parse","HEAD"])
+
+if __name__ == '__main__':
+    from CRABAPI.RawCommand import crabCommand
+    from CRABClient.ClientExceptions import ClientException
+    from httplib import HTTPException
+    
+    def submit(config,crabCmdOpts):
+        config.General.requestName = config.General.requestName+'-v'+str(newVersion)
+        config.Data.outputDatasetTag = config.Data.outputDatasetTag+'-v'+str(newVersion)
+        try:
+            crabCommand('submit',config=config,*crabCmdOpts)
+        except HTTPException as hte:
+            print "Failed submitting task: %s" % (hte.headers)
+            raise GenericSubmissionException
+        except ClientException as cle:
+            print "Failed submitting task: %s" % (cle)
+            raise GenericSubmissionException
+
+    import ntp.NtpAnalyzer.crab as crab
+    submitType=raw_input("MC, Data, or Both?\n")
+    isMC=(submitType=="MC" or submitType=="Both")
+    isData=(submitType=="Data" or submitType=="Both")
+
+    mcSubmit=False
+    dataSubmit=False  
+    mcDirectory=''
+    dataDirectory=''
+
+    if isMC:
+        mcDirectory=interactiveOptionsSubmit(crab.MC(crab.defaultConfig),"MC")
+        mcSubmit=bool(mcDirectory)
+
+    if isData:
+        dataDirectory=interactiveOptionsSubmit(crab.Data(crab.defaultConfig),"Data")
+        dataSubmit=bool(dataDirectory)
+    metaData=(newHash,isMC,mcSubmit,isData,dataSubmit,mcDirectory,dataDirectory)
+    versions['v'+str(newVersion)]=metaData
+    f=open("versionControl.p",'w')
+    pickle.dump(versions,f)
